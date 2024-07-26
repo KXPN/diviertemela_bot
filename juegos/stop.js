75315912ff7f6830f20d563bb24c7dbd5cf533b5
Carlos Piyin Tejada <piyinternet@outlook.com>
2024-07-25 20:54:01 -0500

    Agrega instrucciones al stop.js

--- juegos/stop.js
+++ juegos/stop.js
@@ -1,5 +1,52 @@
 const letrasLineasPorLetra = require('./letrasLineasPorLetra');
-const stop = function(t, evento) {
+
+function enviarInstrucciones(t, evento) {
+        const mensajeLineas = [
+                'STOP',
+                '',
+                'Si ya sabes cómo jugar, puedes recibir la primera letra con →',
+                '/stop_iniciar, sino estas son las instrucciones:',
+                '',
+                '»1. Escribe las categorías en algún lugar, lo más común es →',
+                'una hoja, y algunas cateogrías podrían ser:',
+                '»- Animal',
+                '»- Comida',
+                '»- Famoso',
+                '»- Lugar',
+                '»- Objeto',
+                '»o incluso podrías usar frases, algunas podrían ser:',
+                '»- Llegué tarde por',
+                '»- Mi papá me abandonó por',
+                '»- Prefiero que me',
+                '»',
+                '»2. Escoges una letra (aquí es cuando entro yo) y con ella →',
+                'debes iniciar todas las respuestas',
+                '»',
+                '»3. El primer jugador en haber llenado todas las categorías →',
+                'dice STOP y los demás deben dejar de escribir. Luego lee →',
+                'cada categoría y cada jugador, incluyéndolo, lee lo que →',
+                'escribió en ella. Los puntajes se suelen manejar así:',
+                '»- 200 puntos para una sola persona si llenó correctamente la →',
+                'categoría que nadie más llenó',
+                '»- 100 puntos para todas las personas que hayan llenado →',
+                'correctamente la categoría, con una respuesta única (que →',
+                'nadie más escribió)',
+                '»- 50 puntos para todas las personas que hayan llenado →',
+                'correctamente la categoría, pero su respuesta también fue →',
+                'escrita por alguien más',
+                '»',
+                '»4. Al finalizar de escribir los puntos, cada jugador suma →',
+                'los suyos',
+                '»',
+                '»5. Me pides otra letra para seguir a la siguiente ronda',
+                '»',
+                '»Al iniciar te enviaré una letra aleatoria sin repetición',
+                '»/stop_iniciar«',
+        ];
+        t.enviarMensajeLineas(evento, mensajeLineas);
+}
+
+function enviarSiguienteLetraSinRepeticion(t, evento) {
         const argumentos = t.obtenerArgumentos(evento);
         const letrasExcluidas = (argumentos[1] || '').toUpperCase().split('');
         const letrasExcluidasPorLetra = {};
@@ -36,20 +83,40 @@ const stop = function(t, evento) {
                 );
                 mensajeContinuacionLineas = [
                         '/stop',
+                        '...para ver de nuevo las instrucciones',
+                        '',
+                        '/stop_iniciar',
                         '...para reiniciar el juego',
                         '',
-                        ('/stop\\_' + letrasExcluidasTexto),
+                        ('/stop_' + letrasExcluidasTexto),
                         '...para obtener otra letra sin repetir',
                 ];
         }
         const letraNumero = (letrasExcluidasCantidad + 1);
         const mensajeLineas = [
                 (letraSeleccionada + ' - Letra #' + letraNumero),
+                '',
                 '```',
                 ...letrasLineasPorLetra[letraSeleccionada],
                 '```',
+                '',
                 ...mensajeContinuacionLineas,
         ];
         t.enviarMensajeLineas(evento, mensajeLineas);
+}
+
+const stop = function(t, evento) {
+        const argumentos = t.obtenerArgumentos(evento);
+        const primerArgumento = (argumentos[1] || '');
+        switch (primerArgumento) {
+                case '':
+                        enviarInstrucciones(t, evento);
+                        break;
+                case 'iniciar':
+                        t.borrarArgumentos(evento);
+                default:
+                        enviarSiguienteLetraSinRepeticion(t, evento);
+        }
 };
+
 module.exports = stop;
